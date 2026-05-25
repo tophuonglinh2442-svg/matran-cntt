ff65cc38cfb0b52b3d09256d1464aae45e89ce55
tophuonglinh2442-svg <tophuonglinh2442@gmail.com>
2026-05-25 08:51:58 +0700

    Delete tdom.js

--- tdom.js
+++
@@ -1,5 +0,0 @@
-//var theh1 = document.getElementById("title");
-//console.log(theh1.innerHTML);
-//theh1.innerHTML = "0 ok";
-var theh1 = document.getElementsByTagName("h1");
-console.log(theh1[0].innerHTML);
