c396ca6c8dc0dcd791c51fe4903dbd5e65658153
tophuonglinh2442-svg <tophuonglinh2442@gmail.com>
2026-05-25 08:53:19 +0700

    Delete bai3.js

--- bai3.js
+++
@@ -1,8 +0,0 @@
-var year = parseInt(prompt("Nhập vào năm:"));
-
-var nhuan = year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
-if (nhuan) {
-  document.write("Là năm nhuận");
-} else {
-  document.write("Năm không nhuận");
-}
