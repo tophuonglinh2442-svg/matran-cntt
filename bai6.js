9120358920eebc6fe0f7bd5af7eed95aea50f037
tophuonglinh2442-svg <tophuonglinh2442@gmail.com>
2026-05-25 08:55:03 +0700

    Delete bai6.js

--- bai6.js
+++
@@ -1,9 +0,0 @@
-let phut = Number(prompt("số phút là:"));
-let price = 25000;
-if (phut == 50) {
-  console.log("price" + parseFloat(600 + price));
-} else if (phut == 150) {
-  console.log("price" + parseFloat(400 + price));
-} else if (phut == 200) {
-  console.log("price" + parseFloat(200 + price));
-}
